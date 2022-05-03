39dc0e86e1557929e2fb62f914c15c9d8d0df460
Felipe Rangel <feliperangel277@gmail.com>
2022-05-03 18:24:49 -0300

    Corrige erro de email

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -9,8 +9,10 @@ export default function Profile() {
 
   useEffect(() => {
     const checkData = () => {
-      const { email } = JSON.parse(localStorage.getItem('user'));
-      setUserEmail(email);
+      if (localStorage.getItem('user') !== null) {
+        const user = JSON.parse(localStorage.getItem('user'));
+        setUserEmail(user.email);
+      }
     };
     checkData();
   }, []);
